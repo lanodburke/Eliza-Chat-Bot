a7110cd1053df92f10965b1b5e3e2027223e46be
Donal Burke <dbrke98@gmail.com>
2017-11-23 00:51:59 +0000

    added reflections, and initial and quit messages.

--- eliza.go
+++ eliza.go
@@ -14,6 +14,7 @@ import (
 	"math/rand"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
 type keywords struct {
@@ -34,6 +35,20 @@ var defualtResponses = []string{
 	"I see. And what does that tell you?",
 	"How does that make you feel?",
 	"How do you feel when you say that?",
+}
+
+var initialMessages = []string{
+	"Hello How are you feeling today?",
+	"How do you do. Please tell me your problem.",
+	"Please tell me what's been bothering you.",
+}
+
+var quitMessages = []string{
+	"goodbye",
+	"I have to leave",
+	"Goodbye",
+	"See ya latar alligator!",
+	"Our time is up, if you would like to continue that will be another $150.",
 }
 
 var reflections = map[string]string{
@@ -68,12 +83,23 @@ func parseKeywords() keywords {
 func askEliza(input string) string {
 	list := parseKeywords()
 
+	input = strings.TrimRight(input, "\n.!")
+	input = strings.ToLower(input)
+
 	for _, responses := range list.Keyword {
 		re := regexp.MustCompile(responses.Word)
 		matches := re.FindStringSubmatch(input)
 
 		if len(matches) > 0 {
+			var frag string
+			if len(matches) > 1 {
+				frag = reflection(matches[1])
+			}
+
 			response := randChoice(responses.Responses)
+			if strings.Contains(response, "%s") {
+				response = fmt.Sprintf(response, frag)
+			}
 
 			return response
 		}
@@ -84,6 +110,16 @@ func askEliza(input string) string {
 func randChoice(list []string) string {
 	randIndex := rand.Intn(len(list))
 	return list[randIndex]
+}
+
+func reflection(input string) string {
+	s := strings.Split(input, " ")
+	for i, str := range s {
+		if reflected, ok := reflections[str]; ok {
+			s[i] = reflected
+		}
+	}
+	return strings.Join(s, " ")
 }
 
 func chatWindow(w http.ResponseWriter, r *http.Request) {
